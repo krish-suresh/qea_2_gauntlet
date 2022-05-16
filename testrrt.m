06bf1f0e8ba03a2671a8052992b31c420deaeda9
Krishna Suresh <krishna4132@gmail.com>
2022-05-16 17:52:44 -0400

    final

--- testrrt.m
+++ testrrt.m
@@ -1,12 +1,12 @@
 
 map = gauntletMap();
-map_original = map;
+map_original = gauntletMap;
 inflate(map, 0.15)
 
 start = [0,0];
 goal = [0.75,-2.5];
 goalThresh = 0.3;
-maxNodes = 100;
+maxNodes = 400;
 maxDist = 0.3;
 
 [reached, pathToGoal, G] = rrt(map,start,goal,goalThresh,maxNodes,maxDist);
